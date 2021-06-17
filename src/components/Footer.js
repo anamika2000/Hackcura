8069cd6db159452c9fd772e08d86d27e250711a6
Abhishek Pratap Singh Chauhan <chauhanabhi0098@gmail.com>
2021-06-17 22:21:28 +0530

    Added a minor change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,12 +4,12 @@ import image_footer from './images/footerimg.png';
 
 const Footer = (props) => {
   return(
-    <div className="container-fluid">
-        <div className="row background">
+    <div className="container-fluid background">
+        <div className="row ">
             <div className="col-lg-6 col-5 mx-auto">
                 <div className="my-5 mx-lg-0">
                     <h2 style={{color:"#ffffff"}}>Follow <span style={{color:"#20C20E"}}> Us</span></h2>
-                    <i className="fas fa-user"></i>
+                    <p style={{color:'white'}}>Hackcura Private limited<br/>New Delhi, 110005</p>
                 </div>
             </div>
             <div className="col-lg-4 d-none d-lg-block">
@@ -18,6 +18,9 @@ const Footer = (props) => {
                 </div>
             </div>
             <div className="col-lg-1"></div>
+        </div>
+        <div className ='row justify-content-center'>
+            <p className ="copyr">© Copyright 2021, Hackcura Pvt ltd.</p>
         </div>
     </div>
    )
