1ff8162260cd8b1f61284b4868a196e7832adbd0
Abhishek Pratap Singh Chauhan <chauhanabhi0098@gmail.com>
2021-07-13 21:38:57 +0530

    map embedded

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -10,20 +10,20 @@ const ContactUs = () => {
         <div className="col-lg-6 col-12">
           <h1>CONTACT <span style={{color:"#20C20E"}}>US</span></h1>
           <form>
-            <div className="form-floating mt-lg-5 mt-5">
-              <input type="text" className="form-control input_boxes" id="floatingInput" placeholder="Name"/>
+            <div class="form-floating mt-lg-5 mt-5">
+              <input type="text" class="form-control input_boxes" id="floatingInput" placeholder="Name"/>
               <label htmlFor="floatingInput">Name*</label>
             </div>
-            <div className="form-floating mt-lg-4 mt-3">
-              <input type="email" className="form-control input_boxes" id="floatingInput" placeholder="name@example.com"/>
+            <div class="form-floating mt-lg-4 mt-3">
+              <input type="email" class="form-control input_boxes" id="floatingInput" placeholder="name@example.com"/>
               <label htmlFor="floatingInput">Email address*</label>
             </div>
-            <div className="form-floating mt-lg-4 mt-3">
-              <input type="text" className="form-control input_boxes" id="floatingInput" placeholder="9140716702"/>
+            <div class="form-floating mt-lg-4 mt-3">
+              <input type="text" class="form-control input_boxes" id="floatingInput" placeholder="9140716702"/>
               <label htmlFor="floatingInput">Phone no. (with country code)</label>
             </div>
-            <div className="form-floating mt-lg-4 mt-3">
-              <textarea type="text" className="form-control input_boxes" id="floatingTextarea2" placeholder="text" style={{minHeight:"200px"}}/>
+            <div class="form-floating mt-lg-4 mt-3">
+              <textarea type="text" class="form-control input_boxes" id="floatingTextarea2" placeholder="text"/>
               <label htmlFor="floatingTextarea2">Query or Reason*</label>
             </div>
             <div className="text-lg-start text-center">
@@ -47,9 +47,9 @@ const ContactUs = () => {
         </div>
       </div>
       <div className="row my-4">
-        <div className="col-lg-12 col-10 bg-danger text-white mx-auto" style={{borderRadius:"20px"}}>
+        <div className="col-lg-8 col-10 bg-danger text-white mx-auto" style={{borderRadius:"20px"}}>
           <h3 className="text-center py-4">HACKCURA VULNERABILITY DISCLOSURE PROGRAM</h3>
-          <p className="text-center">If you come accross any security vulnerabilities (Critical, High, Medium or Low) in any of the asset of Hackcura, Please feel free to report it on<br/><span style={{fontWeight:'600'}}>Mail to : support@hackcura.in</span><br/>And claim Hackcura swag of Tshirt or sticker on every unique vulnerability reported.</p>
+          <p className="text-center">If you come accross any security vulnerabilities (Critical, High, Medium or Low) in any of the asset of Hackcura, Please feel free to report it on<br/><span style={{fontWeight:'600'}}>Mail to : support@hackcura.in</span><br/>And claim Hackcura swag of Tshit or sticker on every unique vulnerability reported.</p>
         </div>
       </div>
     </section>
@@ -57,4 +57,4 @@ const ContactUs = () => {
 
  }
 
-export default ContactUs;
+export default ContactUs
