efbb51df67af397f63b9b78ac45b73fb11d4d4c5
Abhishek Pratap Singh Chauhan <chauhanabhi0098@gmail.com>
2021-07-26 22:06:26 +0530

    Added cards

--- src/components/Career.js
+++ src/components/Career.js
@@ -3,6 +3,10 @@ import './style/Career.css';
 import { BiSearchAlt } from "react-icons/bi";
 import { VscSymbolKeyword } from "react-icons/vsc";
 import { GoLocation } from "react-icons/go";
+import { NavLink } from 'react-router-dom';
+
+
+// sample data for jobs:
 
 
 
@@ -16,17 +20,100 @@ const Career = () => {
         </div>
         <div className="row my-4">
             <form className="d-lg-flex text-center">
-                <div class="input-group">
-                    <span class="input-group-text">Keyword<VscSymbolKeyword className="ms-1" style={{color:"#20C20E"}}/></span>
-                    <input type="text" aria-label="First name" class="form-control text-center" placeholder="Intern, Content writer etc"/>
+                <div className="input-group">
+                    <span className="input-group-text">Keyword<VscSymbolKeyword className="ms-1" style={{color:"#20C20E"}}/></span>
+                    <input type="text" aria-label="First name" className="form-control text-center" placeholder="Intern, Content writer etc"/>
                 </div>
-                <div class="input-group ms-lg-5 mt-lg-0 mt-3">
-                    <span class="input-group-text">Location <GoLocation className="ms-1" style={{color:"#20C20E"}}/></span>
-                    <input type="text" aria-label="First name" class="form-control text-center" placeholder="Remote, New Delhi, Noida etc"/>
+                <div className="input-group ms-lg-5 mt-lg-0 mt-3">
+                    <span className="input-group-text">Location <GoLocation className="ms-1" style={{color:"#20C20E"}}/></span>
+                    <input type="text" aria-label="First name" className="form-control text-center" placeholder="Remote, New Delhi, Noida etc"/>
                 </div> 
-                <button class="btn btn-success ms-lg-2 mt-2 mt-lg-0 ps-4 pe-4"><BiSearchAlt/></button>
+                <button className="btn btn-success ms-lg-2 mt-2 mt-lg-0 ps-4 pe-4"><BiSearchAlt/></button>
             </form>
         </div>
+        <div className="row">
+            <div className="col-lg-3 col-10 mx-auto my-lg-4 my-3">
+                <div className="card text-center">
+                    <div className="card-header">
+                        Internship
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">ReactJs Developer</h5>
+                        <div className="card-text my-4">
+                            <p>5+ years of experience</p>
+                            <p>Smart working</p>
+                            <p>comfortable with 10 to 7 job</p>  
+                        </div>
+                        <NavLink to="/contactus" className="btn btn-success">Go somewhere</NavLink>
+                    </div>
+                    <div className="card-footer text-muted">
+                        2 days ago
+                    </div>
+                </div>
+                
+            </div>
+            <div className="col-lg-3 col-10 mx-auto my-lg-4 my-3">
+                <div className="card text-center">
+                    <div className="card-header">
+                        FTE
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">CyberSecurity Expert</h5>
+                        <div className="card-text my-4">
+                            <p>5+ years of experience</p>
+                            <p>Smart working</p>
+                            <p>comfortable with 10 to 7 job</p>  
+                        </div>
+                        <NavLink to="/contactus" className="btn btn-success">Go somewhere</NavLink>
+                    </div>
+                    <div className="card-footer text-muted">
+                        11 days ago
+                    </div>
+                </div>
+                
+            </div>
+            <div className="col-lg-3 col-10 mx-auto my-lg-4 my-3">
+                <div className="card text-center">
+                    <div className="card-header">
+                        Internship
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">Data Science</h5>
+                        <div className="card-text my-4">
+                            <p>5+ years of experience</p>
+                            <p>Smart working</p>
+                            <p>comfortable with 10 to 7 job</p>  
+                        </div>
+                        <NavLink to="/contactus" className="btn btn-success">Go somewhere</NavLink>
+                    </div>
+                    <div className="card-footer text-muted">
+                        16 days ago
+                    </div>
+                </div>
+                
+            </div>
+            <div className="col-lg-3 col-10 mx-auto my-lg-4 my-3">
+                <div className="card text-center">
+                    <div className="card-header">
+                        FTE
+                    </div>
+                    <div className="card-body">
+                        <h5 className="card-title">Backend Developer</h5>
+                        <div className="card-text my-4">
+                            <p>5+ years of experience</p>
+                            <p>Smart working</p>
+                            <p>comfortable with 10 to 7 job</p>  
+                        </div>
+                        <NavLink to="/contactus" className="btn btn-success">Go somewhere</NavLink>
+                    </div>
+                    <div className="card-footer text-muted">
+                        19 days ago
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    
         
     </section>
    )
